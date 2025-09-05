e31a420b54cf5c03c86ea767a3e990483be2d34d
Shiloh <you@example.com>
2025-09-04 21:52:57 -0400

    analytics + speedinsights

--- PromptSculptor/client/src/App.tsx
+++ PromptSculptor/client/src/App.tsx
@@ -4,6 +4,8 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { AuthProvider } from "@/context/AuthContext";
 import { AdminAuthProvider } from "@/context/AdminAuthContext";
 import { ThemeProvider } from "@/context/ThemeContext";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/home";
 import Favorites from "@/pages/favorites";
@@ -87,6 +89,8 @@ function App() {
           <TooltipProvider>
             <Toaster />
             <Router />
+            <Analytics />
+            <SpeedInsights />
           </TooltipProvider>
         </AdminAuthProvider>
       </AuthProvider>
